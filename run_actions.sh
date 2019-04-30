d1ed45d07cd729873e6b18dd08d36e3d35975f94
Pim van Gennip <pim@iconize.nl>
2019-05-01 00:02:07 +0200

    Added bitnami apache vhosts

--- run_actions.sh
+++ run_actions.sh
@@ -5,7 +5,7 @@ base_dir=$(pwd)
 # laravel
 cd $base_dir
 if [ ! -f '.env' ]; then cp .env.example .env && php artisan key:generate; fi
-composer install && sudo chmod -R 777 storage && sudo chmod -R 777 bootstrap/cache && php artisan migrate && php artisan vendor:publish && php artisan storage:link
+composer install && sudo chmod -R 777 storage && sudo chmod -R 777 bootstrap/cache && php artisan migrate  && php artisan storage:link
 composer dumpautoload
 
 # bower

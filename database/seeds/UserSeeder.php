8d25abe46d69e606b6666871b924421302d0b521
Pim van Gennip <pim@iconize.nl>
2019-05-09 12:16:40 +0200

    Update UserSeeder.php

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
         $users = User::all();
         foreach ($users as $user) 
         {
-        	if (isset($user->policy_accepted) && $user->email_verified_at == null)
+            if (isset($user->policy_accepted) && $user->email_verified_at == null)
             {
                 $user->email_verified_at = "2018-05-25 00:00:00";
                 $user->save();
@@ -24,3 +24,4 @@ class UserSeeder extends Seeder
         }
     }
 }
+

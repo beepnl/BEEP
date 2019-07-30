01d9b2e81c3f57e36e6820d20a94bba2b855b7ff
Pim van Gennip <pim@iconize.nl>
2019-07-30 10:09:58 +0200

    Added crontab backup

--- backup.sh
+++ backup.sh
@@ -6,9 +6,10 @@ sudo mkdir backups/mysql
 sudo mkdir backups/influx
 
 echo "Backing up MySQL database"
+INFLUX_PASS=$(grep -oP '^LARAVEL_INFLUX_PROVIDER_PASSWORD=\K.*' .env)
 sudo mv backups/mysql/bee_base.sql.gz-1 backups/mysql/bee_base.sql.gz-2
 sudo mv backups/mysql/bee_base.sql.gz backups/mysql/bee_base.sql.gz-1
-mysqldump -u beep -p'qLHYrFhkaYEM9r7L' bee_base | gzip > backups/mysql/bee_base.sql.gz
+mysqldump -u beep -p'$INFLUX_PASS' bee_base | gzip > backups/mysql/bee_base.sql.gz
 
 echo "Backing up Influx database"
 DATE=$(date +"%Y-%m-%d")

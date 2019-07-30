8627cc6775baaa7f3bf6a4f244abe528aba743d7
Pim van Gennip <pim@iconize.nl>
2019-07-30 10:57:10 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,20 +1,45 @@
  #!/bin/sh 
 
+DATE=$(date +"%Y-%m-%d")
+
 cd ~/
-sudo mkdir backups
-sudo mkdir backups/mysql
-sudo mkdir backups/influx
+if [ ! -d "backups" ]
+then
+	sudo mkdir backups
+fi
+
+if [ ! -d "backups/mysql" ]
+then
+	sudo mkdir backups/mysql
+fi
+
+if [ ! -d "backups/influx" ]
+then
+	sudo mkdir backups/influx
+fi
 
 echo "Backing up MySQL database"
-INFLUX_PASS=$(grep -oP '^LARAVEL_INFLUX_PROVIDER_PASSWORD=\K.*' .env)
-sudo mv backups/mysql/bee_base.sql.gz-1 backups/mysql/bee_base.sql.gz-2
-sudo mv backups/mysql/bee_base.sql.gz backups/mysql/bee_base.sql.gz-1
-mysqldump -u beep -p'$INFLUX_PASS' bee_base | gzip > backups/mysql/bee_base.sql.gz
+MDIR="backups/mysql/$DATE"
+MYSQL_PASS=$(grep -oP '^DB_PASSWORD=\K.*' apps/BEEP/.env)
+mysqldump -u beep -p"$MYSQL_PASS" bee_base | gzip > backups/mysql/bee_base.sql.gz
+if [ -d "$MDIR" ]
+then
+	sudo rm -rf $MDIR
+fi
+sudo mkdir $MDIR
+sudo cp backups/mysql/bee_base.sql.gz $MDIR
 
 echo "Backing up Influx database"
-DATE=$(date +"%Y-%m-%d")
 IDIR="backups/influx/$DATE"
+
+if [ -d "$IDIR" ]
+then
+	sudo rm -rf $IDIR
+fi
 sudo mkdir $IDIR
 influxd backup -database bee_data -retention autogen $IDIR
+
+sudo rm backups/influx/*.00
+sudo cp $IDIR/*.00 backups/influx/
 
 exit 0

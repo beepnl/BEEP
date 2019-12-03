64f56ea8e33085ed697720155315e9ee8c2431f4
Pim van Gennip <pim@iconize.nl>
2019-12-03 12:16:02 +0000

    Add support for multi weight and temperature sensors

--- ttn-payload-decoder.js
+++ ttn-payload-decoder.js
@@ -50,18 +50,48 @@ function Decoder(bytes, port) {
       decoded.long   = true;
     }
   }
-  else if (port == 3 && bytes.length == 15)  // BEEP base fw 1.2.0, 1 weight, 1 temp
+  else if (port == 3 && bytes.length >= 15)  // BEEP base fw 1.2.0, 1+ weight, 1+temp
   {
     // TODO: Make dynamic based on multi weight and multi temp
     
     // raw pl  1B0C4B0C44640A01012D2D040107D6
     // Payload 1B 0C4B0C4464 0A 01 012D2D 04 01 07D6
     //         6  batt       5  1 weight  5 1-5 temp (1 to 5)
+    
+    // Battery 
+    decoded.bv = (bytes[3] << 8) + bytes[4]; // 1B (0 batt) 0C4B (1-2 vcc) 0C44 (3-4 vbat) 64 (5 %bat)
 
-    decoded.bv    =  (bytes[3] << 8) + bytes[4]; // 1B (0 batt) 0C4B (1-2 vcc) 0C44 (3-4 vbat) 64 (5 %bat)
-    decoded.w_v   =  (bytes[8] << 16) + (bytes[9] << 8) + bytes[10]; // 0A (6 weight) 01 (7 1x) 012D2D (8-10 value)
-    decoded.t_i   =  (bytes[13] << 8) + bytes[14]; // 04 (11 temp) 01 (12 1x) 07D6 (13-14 value)
+    decoded.amount_of_weight_sensors = bytes[7];
+    
+    // Weight
+    var weight_byte_length       = 3;
+    if (decoded.amount_of_weight_sensors == 1)
+    {
+      decoded.w_v   =  (bytes[8] << 16) + (bytes[9] << 8) + bytes[10]; // 0A (6 weight) 01 (7 1x) 012D2D (8-10 value)
+    }
+    else
+    {
+      for(var i = 0; i < decoded.amount_of_weight_sensors; i++)
+      {
+        var valueByteIndex = 8 + (i * weight_byte_length); 
+        decoded['w_v_' + i] = (bytes[valueByteIndex] << 16) + (bytes[valueByteIndex+1] << 8) + bytes[valueByteIndex+2]; 
+      }
+    }
+
+    decoded.amount_of_temperature_sensors = bytes[9 + (decoded.amount_of_weight_sensors * weight_byte_length)];
+    var temperature_byte_length       = 2;
+    if (decoded.amount_of_temperature_sensors == 1)
+    {
+      decoded.t_i =  (bytes[13] << 8) + bytes[14]; // 04 (11 temp) 01 (12 1x) 07D6 (13-14 value)
+    }
+    else
+    {
+      for(var j = 0; j < decoded.amount_of_temperature_sensors; j++)
+      {
+        var tempValueByteIndex = 8 + (j * temperature_byte_length); 
+        decoded['t_' + j] = (bytes[tempValueByteIndex] << 8) + bytes[tempValueByteIndex+1]; 
+      }
+    }
   }
-
   return decoded;
 }

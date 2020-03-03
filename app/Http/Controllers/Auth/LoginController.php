edec95436f67aa6cfd9a60d74ef5a27995b9e7e9
Pim van Gennip <pim@iconize.nl>
2020-03-03 21:05:30 +0100

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function logout() 
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
 }

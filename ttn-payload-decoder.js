c60ed23dbacf856b308eb345b04efd357341c00a
Pim van Gennip <pim@iconize.nl>
2020-03-10 18:21:58 +0100

    Update ttn-payload-decoder.js

--- ttn-payload-decoder.js
+++ ttn-payload-decoder.js
@@ -15,13 +15,13 @@ function Decoder(bytes, port) {
   }
 
   function hexToInt(hex) {
-    if (hex.length % 2 != 0) {
+    if (hex.length % 2 !== 0) {
         hex = "0" + hex;
     }
     var num = parseInt(hex, 16);
     var maxVal = Math.pow(2, hex.length / 2 * 8);
     if (num > maxVal / 2 - 1) {
-        num = num - maxVal
+        num = num - maxVal;
     }
     return num;
 }

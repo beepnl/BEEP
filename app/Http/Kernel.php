61043ccc1cefb60a4ebd164cfde69c6b296ad826
Pim van Gennip <pim@iconize.nl>
2020-03-12 13:32:02 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\Cors::class,
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 

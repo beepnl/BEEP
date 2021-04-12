c585049b101d7cf845a9f62a891916068bc84cb4
Pim van Gennip <pim@iconize.nl>
2021-04-12 05:28:03 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -22,21 +22,23 @@ MYSQL_USER=$(/bin/grep -oP '^DB_USERNAME=\K.*' /home/bitnami/apps/BEEP/.env)
 MYSQL_PASS=$(/bin/grep -oP '^DB_PASSWORD=\K.*' /home/bitnami/apps/BEEP/.env)
 /opt/bitnami/mysql/bin/mysqldump -u"$MYSQL_USER" -p"$MYSQL_PASS" bee_base | /bin/gzip > /home/bitnami/backups/mysql/bee_base-$DATE.gz
 
-echo "Remove MySQL backups older than 60 days"
-sudo find /home/bitnami/backups/mysql/* -mtime +60 -delete
+echo "Remove MySQL backups older than 14 days"
+sudo find /home/bitnami/backups/mysql/* -mtime +14 -delete
 
 # Sync todays backup to S3 (delete all available files there, and only sync the files inside the root folder + all folders that are named like the first of the month)
-/usr/bin/aws s3 sync /home/bitnami/backups/mysql/ s3://beeplegacyinfrastructurestack-bucket83908e77-1088rn21ai0um/backups/mysql --exclude "*" --include "*.gz" --delete
+echo "Upload MySQL bee_base-$DATE.gz to s3"
+/usr/bin/aws s3 cp "/home/bitnami/backups/mysql/bee_base-$DATE.gz" "s3://beep-database-backups/mysql/bee_base-$DATE.gz"
 
 echo "Backing up Influx database"
 influxd backup -database bee_data -retention autogen /home/bitnami/backups/influx
 sudo rm /home/bitnami/backups/influx/*.pending
 
-echo "Compress Influx backup to file and remove backups older than 30 days"
-/bin/tar -czf "/home/bitnami/backups/influx/bee_data-$DATE-influx.gz" /home/bitnami/backups/influx/*.00 && sudo rm /home/bitnami/backups/influx/*.00 && sudo find /home/bitnami/backups/influx/* -mtime +30 -delete
+echo "Compress Influx backup to file and remove backups older than 14 days"
+/bin/tar -czf "/home/bitnami/backups/influx/bee_data-$DATE-influx.gz" /home/bitnami/backups/influx/*.00 && sudo rm /home/bitnami/backups/influx/*.00 && sudo find /home/bitnami/backups/influx/* -mtime +14 -delete
 
 # Sync todays backup to S3 (option: delete all available files there, and only sync the files inside the home/bitnami folder + all folders that are named like the first of the month: --exclude "*/*" --include "????-??-01/*")
-/usr/bin/aws s3 sync /home/bitnami/backups/influx/ s3://beeplegacyinfrastructurestack-bucket83908e77-1088rn21ai0um/backups/influx --exclude "*" --include "*.gz" --delete
+echo "Upload Influx bee_data-$DATE-influx.gz to s3"
+/usr/bin/aws s3 cp "/home/bitnami/backups/influx/bee_data-$DATE-influx.gz" "s3://beep-database-backups/influx/bee_data-$DATE-influx.gz"
 
 echo "Finished backup for $DATE"
 echo ""

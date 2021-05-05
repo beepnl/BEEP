c32d2b7b98b86d28e31720c7826f8bee84e12f20
Pim van Gennip <pim@iconize.nl>
2021-05-05 17:52:04 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 use App\Weather;
+use App\Models\AlertRule;
 
 class Kernel extends ConsoleKernel
 {

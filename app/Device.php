e4521adae2a210a98d5ef619b2f72c853c64d21f
Pim van Gennip <pim@iconize.nl>
2021-05-06 09:20:12 +0200

    Correct iOS app sensor definition interpretation order

--- app/Device.php
+++ app/Device.php
@@ -54,7 +54,7 @@ class Device extends Model
 
     public function sensorDefinitions()
     {
-        return $this->hasMany(SensorDefinition::class);
+        return $this->hasMany(SensorDefinition::class)->orderByDesc('updated_at');
     }
 
 	public function hive()

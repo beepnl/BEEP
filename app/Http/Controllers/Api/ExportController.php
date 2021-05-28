97a6667c2b3847d6e96a4fb6186a8666055e3238
Pim van Gennip <pim@iconize.nl>
2021-05-28 16:10:03 +0200

    Bugfix data export

--- app/Http/Controllers/Api/ExportController.php
+++ app/Http/Controllers/Api/ExportController.php
@@ -293,7 +293,7 @@ class ExportController extends Controller
             else 
                 $groupBySelect = '"'.implode('","',$names).'"';
 
-        $query = 'SELECT '.$groupBySelect.' FROM "sensors" WHERE '.$whereDeviceTime.' '.$groupByResolution;
+        $query = 'SELECT '.$groupBySelect.' FROM "sensors" WHERE '.$whereDeviceTime;
         
         try{
             $data   = $this->client::query($query, $options)->getPoints(); // get first sensor date

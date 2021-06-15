d28dbc5c8708e2746837106d45da1c7243040cf6
Pim van Gennip <pim@iconize.nl>
2021-06-15 17:59:45 +0200

    Update weather data every 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
             {
                 Weather::updateLocations();
             }
-        )->everyMinute();
+        )->everyFiveMinutes();
     }
 
     /**

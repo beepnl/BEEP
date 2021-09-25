e2d45d2c86992749f7eda484c35ba39afd716d47
Pim van Gennip <pim@iconize.nl>
2021-09-25 09:16:22 +0200

    Add user to Alerts

--- resources/views/alert/index.blade.php
+++ resources/views/alert/index.blade.php
@@ -49,11 +49,12 @@
                     <th>Alert Rule</th>
                     <th>Alert Function</th>
                     <th>Alert Value</th>
-                    <th>Measurement (Id)</th>
+                    <th>Measurement</th>
+                    <th>User</th>
                     <th>Count</th>
-                    <th>Location Name</th>
-                    <th>Hive Name</th>
-                    <th>Device Name</th>
+                    <th>Location</th>
+                    <th>Hive</th>
+                    <th>Device</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -67,10 +68,11 @@
                     <td>{{ $item->alert_function }}</td>
                     <td>{{ $item->alert_value }}</td>
                     <td>{{ $item->measurement->pq_name_unit }} ({{ $item->measurement->id }})</td>
+                    <td>{{ $item->user_id }}</td>
                     <td>{{ $item->count }}</td>
                     <td>{{ $item->location_name }}</td>
                     <td>{{ $item->hive_name }}</td>
-                    <td>{{ $item->device_name }}</td>
+                    <td>{{ $item->device_name }} ({{ $item->device_id }})</td>
                     <td col-sm-1>
                         <a href="{{ route('alert.show', $item->id) }}" title="{{ __('crud.show') }}"><button class="btn btn-default"><i class="fa fa-eye" aria-hidden="true"></i></button></a>
 

e3ca81681c1a7715e2950da5b09bf4122e6922cb
Pim van Gennip <pim@iconize.nl>
2021-09-29 12:33:10 +0200

    Parse Alerts every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends ConsoleKernel
             {
                 AlertRule::parseRules();
             }
-        )->everyFiveMinutes();
+        )->everyMinute();
     }
 
     /**

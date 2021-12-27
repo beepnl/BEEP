e3460bd896a4f899d27cafd31d0a7e4103534770
Pim van Gennip <pim@iconize.nl>
2021-12-27 13:15:48 +0100

    Only list invited that have not been accepted, or declined and list the correct (accepted) amount of users

--- app/User.php
+++ app/User.php
@@ -225,14 +225,14 @@ class User extends Authenticatable
     public function groupInvitations()
     {
         $user_id = $this->id;
-        return $this->belongsToMany(Group::class, 'group_user')->whereNotNull('invited')->whereNull('accepted')->get()->map(function ($item, $key) use ($user_id)
+        return $this->belongsToMany(Group::class, 'group_user')->whereNotNull('invited')->whereNull('accepted')->whereNull('declined')->get()->map(function ($item, $key) use ($user_id)
         {
             $invite              = $item->only(['id','name','description','color']);
             $groupUserArray      = $item->users->firstWhere('id',$user_id);
             $invite['invited']   = (isset($groupUserArray['invited'])) ? $item->users->firstWhere('id',$user_id)['invited'] : null;
             $invite['token']     = (isset($groupUserArray['token'])) ? $item->users->firstWhere('id',$user_id)['token'] : null;
             $invite['hivecount'] = $item->hives->count();
-            $invite['usercount'] = $item->users->count();
+            $invite['usercount'] = $item->users->whereNotNull('accepted')->count();
             return $invite; 
         });
     }

2e6bd58276a613d8ed78f8eaef4ecf067b04588a
Pim van Gennip <pim@iconize.nl>
2022-01-14 12:34:53 +0100

    System update for using Laravel Forge

--- create_cert_api.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo /opt/bitnami/ctlscript.sh stop apache
-sudo /opt/bitnami/letsencrypt/lego --tls --email="pim@beep.nl" --domains="api.beep.nl" --domains="app.beep.nl" --domains="graph.beep.nl" --path="/opt/bitnami/letsencrypt" run
-sudo /opt/bitnami/ctlscript.sh start apache
--- renew_cert_api.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo /opt/bitnami/ctlscript.sh stop apache
-sudo /opt/bitnami/letsencrypt/lego --tls --email="pim@beep.nl" --domains="api.beep.nl" --domains="app.beep.nl" --domains="graph.beep.nl" --path="/opt/bitnami/letsencrypt" renew --days 90
-sudo /opt/bitnami/ctlscript.sh start apache

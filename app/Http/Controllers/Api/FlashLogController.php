30fdf6f4022e220e50801b7ff552e74466fec768
Pim van Gennip <pim@iconize.nl>
2022-01-24 16:25:41 +0100

    Add flashlog block data

--- app/Http/Controllers/Api/FlashLogController.php
+++ app/Http/Controllers/Api/FlashLogController.php
@@ -166,6 +166,53 @@ class FlashLogController extends Controller
         return $object;
     }
 
+    private function matchPercentage($array1, $array2, $match_props=9)
+    {
+        $matches       = [];
+        $secDiff       = [];
+        $match_count   = 0;
+        $array2_index  = 0;
+        $array1_length = count($array1);
+        $array2_length = count($array2);
+
+        for ($i=0; $i < $array1_length; $i++) 
+        {
+            $f = (array)$array1[$i];
+            
+
+
+            for ($j=$array2_index; $j < $array2_length; $j++) 
+            {   
+                $d           = array_filter($array2[$j]);
+                $d_val_count = count($d);
+                
+                if ($d_val_count < $match_props)
+                {
+                    $array2_index = $j;
+                    continue;
+                }
+
+                if (isset($f['bv']) && isset($d['bv']) && $f['bv'] == $d['bv']) // first fast check
+                {
+                    $match = array_intersect_assoc($d, $f);
+
+                    if ($match != null && count($match) >= $match_props)
+                    {
+                        $matches[] = ['d'=>$d, 'f'=>$f, 'm'=>$match];
+                        $secDiff[] = strtotime($f['time']) - strtotime($d['time']);
+                        $array2_index = $j;
+                        $match_count++;
+                        continue 2; // next foreach loop to continue with the next database item
+                    }
+                }
+            }
+        }
+        $secDiffAvg = count($secDiff) > 0 ? array_sum($secDiff)/count($secDiff) : null;
+        $percMatch  = $array1_length > 0 ? round(100 * ($match_count / $array1_length), 1): 0;
+        //die(print_r([$percMatch, $secDiffAvg, $matches]));
+        return ['sec_diff'=>$secDiffAvg, 'perc_match'=>$percMatch];
+    }
+
     private function parse(Request $request, $id, $persist=false)
     {
         $matches_min = $request->input('matches_min', env('FLASHLOG_MIN_MATCHES', 2)); // minimum amount of inline measurements that should be matched 
@@ -192,7 +239,9 @@ class FlashLogController extends Controller
             if ($device)
             {
                 $device_id   = $flashlog->device_id;
+                $device_name = $flashlog->device_name;
                 $hive_id     = $flashlog->hive_id;
+                $hive_name   = $flashlog->hive_name;
                 //$measurements= Measurement::getValidMeasurements(true);
                 $measurements = Measurement::getMatchingMeasurements();
                 
@@ -273,9 +322,9 @@ class FlashLogController extends Controller
                                     //die(print_r([$persist_count, $persist_days.' days', $block_start_t, $data_count_d, $missing_data]));
                                     
                                     if ($persist_count > 0)
-                                        $out = ['device_id'=>$device_id, 'hive_id'=>$hive_id, 'block_id'=>$block_id, 'data_stored'=>true, 'persist_count'=>$persist_count, 'persist_days'=>$persist_days];
+                                        $out = ['flashlog_id'=>$id, 'device_id'=>$device_id, 'hive_id'=>$hive_id, 'device_name'=>$device_name, 'hive_name'=>$hive_name, 'block_id'=>$block_id, 'data_stored'=>true, 'persist_count'=>$persist_count, 'persist_days'=>$persist_days];
                                     else
-                                        $out = ['device_id'=>$device_id, 'hive_id'=>$hive_id, 'block_id'=>$block_id, 'data_stored'=>false, 'persist_count'=>$persist_count, 'persist_days'=>0, 'error'=>'data_not_stored'];
+                                        $out = ['flashlog_id'=>$id, 'device_id'=>$device_id, 'hive_id'=>$hive_id, 'device_name'=>$device_name, 'hive_name'=>$hive_name, 'block_id'=>$block_id, 'data_stored'=>false, 'persist_count'=>$persist_count, 'persist_days'=>0, 'error'=>'data_not_stored'];
                                 }
                                 else // Show data content per week
                                 {
@@ -293,7 +342,7 @@ class FlashLogController extends Controller
                                     $start_index = $block_start_i + ($index_amount * $block_data_i);
                                     $end_index   = min($block_end_i, $block_start_i + ($index_amount * ($block_data_i+1)));
 
-                                    $out = ['device_id'=>$device_id, 'hive_id'=>$hive_id, 'block_id'=>$block_id, 'block_start_i'=>$block_start_i, 'block_end_i'=>$block_end_i, 'match_index'=>$match_index, 'block_data_index'=>$block_data_i, 'block_data_index_max'=>$data_i_max, 'block_data_index_amount'=>$index_amount, 'block_data_start'=>$start_index, 'block_data_end'=>$end_index, 'flashlog'=>[], 'database'=>[]];
+                                    $out = ['flashlog_id'=>$id, 'device_id'=>$device_id, 'hive_id'=>$hive_id, 'device_name'=>$device_name, 'hive_name'=>$hive_name, 'block_id'=>$block_id, 'block_start_i'=>$block_start_i, 'block_end_i'=>$block_end_i, 'match_index'=>$match_index, 'block_data_index'=>$block_data_i, 'block_data_index_max'=>$data_i_max, 'block_data_index_amount'=>$index_amount, 'block_data_start'=>$start_index, 'block_data_end'=>$end_index, 'flashlog'=>[], 'database'=>[]];
 
                                     // Add flashlog measurement data
                                     for ($i=$start_index; $i<$end_index; $i++) 
@@ -314,6 +363,11 @@ class FlashLogController extends Controller
                                         $query       = 'SELECT "'.implode('","', $measurements).'" FROM "sensors" WHERE '.$flashlog->device->influxWhereKeys().' AND time >= \''.$start_time.'\' AND time <= \''.$end_time.'\' ORDER BY time ASC LIMIT '.$index_amount;
                                         $out['database'] = Device::getInfluxQuery($query, 'flashlog');
                                     }
+
+                                    // Run through the data to see how many % of the data matches
+                                    $match_percentage = $this->matchPercentage($out['flashlog'], $out['database'], $match_props);
+                                    $out['block_data_match_percentage']  = $match_percentage['perc_match'];
+                                    $out['block_data_flashlog_sec_diff'] = $match_percentage['sec_diff'];
                                 }
                             }
                             else

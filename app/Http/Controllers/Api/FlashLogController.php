a50ac488749af55fe146cc0df1801fc0e1a72eff
Pim van Gennip <pim@iconize.nl>
2022-08-30 16:30:46 +0200

    Update FlashLogController.php

--- app/Http/Controllers/Api/FlashLogController.php
+++ app/Http/Controllers/Api/FlashLogController.php
@@ -442,10 +442,10 @@ class FlashLogController extends Controller
                                 $data_deleted       = 'no_data_to_delete';
                                 $delete_count_query = 'SELECT COUNT("bv") AS "count" FROM "sensors" WHERE "from_flashlog" = \'1\' AND "key" = \''.strtolower($device->key).'\' AND time >= \''.$block_start_t.'\' AND time <= \''.$block_end_t.'\'';
                                 $delete_count       = Device::getInfluxQuery($delete_count_query, 'flashlog');
-                                $delete_count_sum   = isset($delete_count[0]) ? array_sum($delete_count[0]) : 0;
+                                $delete_count_sum   = isset($delete_count[0]['count']) ? $delete_count[0]['count'] : 0;
                                 $deleted_days       = round($delete_count_sum*$interval_min/(60*24), 1);
 
-                                Log::debug(['q'=>$delete_count_query, 'delete_count_items'=>count($delete_count), 'delete_count_sum'=>$delete_count_sum, 'deleted_days'=>$deleted_days]);
+                                Log::debug(['q'=>$delete_count_query, 'delete_count_items'=>count($delete_count), 'delete_count_sum'=>$delete_count_sum, 'deleted_days'=>$deleted_days, 'delete_count'=>$delete_count]);
 
                                 if ($delete_count_sum > 0 && $deleted_days > 0)
                                 {
@@ -458,8 +458,7 @@ class FlashLogController extends Controller
                                     $flashlog->save();
                                 }
                                 
-                                $out = ['data_deleted'=>$data_influx_deleted, 'deleted_measurements'=>$delete_count_sum, 'deleted_days'=>$deleted_days, 'data_deleted_log'=>$data_deleted];
-                                Log::debug($out);
+                                $out = ['data_deleted'=>$data_influx_deleted, 'deleted_measurements'=>$delete_count_sum, 'deleted_days'=>$deleted_days];
                             }
                             else if ($persist) // Save missing data to DB
                             {

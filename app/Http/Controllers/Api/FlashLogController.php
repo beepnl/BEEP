96de633b1df9666d45d1fa1e7d88880f6c9a5798
Pim van Gennip <pim@iconize.nl>
2022-08-30 14:49:31 +0200

    Update FlashLogController.php

--- app/Http/Controllers/Api/FlashLogController.php
+++ app/Http/Controllers/Api/FlashLogController.php
@@ -507,7 +507,6 @@ class FlashLogController extends Controller
                                         $db_count      = array_intersect_key($db_count, $count_measurements); // only keep the key counts from valid matching measurements
                                         $count_sum     = array_sum($db_count);
                                         
-                                        Log::debug(['db_count'=>$db_count, 'c'=>$count_sum, 'match_props'=>$match_props]);
 
                                         $data_per_int_d[$time_start] = $count_sum;
                                         
@@ -520,6 +519,8 @@ class FlashLogController extends Controller
                                             $indexFlogStart  = $block_start_i + ceil($minDifWithStart / $interval_min);
                                             $indexFlogEnd    = min($block_end_i, $indexFlogStart + $rows_per_db);
                                             $indexFlogStart  = max(0, $indexFlogStart);
+                                            
+                                            Log::debug("db_count_i=$db_count_i, count_sum=$count_sum, match_props=$match_props");
 
                                             for ($i=$indexFlogStart; $i < $indexFlogEnd; $i++)
                                             {

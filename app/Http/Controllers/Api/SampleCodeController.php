c4263fbd760b7c41f01f279314614b1b537114f1
Pim van Gennip <pim@iconize.nl>
2022-10-07 11:56:34 +0200

    Update SampleCodeController.php

--- app/Http/Controllers/Api/SampleCodeController.php
+++ app/Http/Controllers/Api/SampleCodeController.php
@@ -29,21 +29,24 @@ class SampleCodeController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    api/samplecode POST
+    Create a sample code for lab results
+    @authenticated
+    **/
     public function store(Request $request)
     {
         if($request->filled('hive_id') && $request->user()->hives()->find($request->input('hive_id')))
         {
-            $code = $request->only('hive_id', 'queen_id');
-            $sample_time = $request->filled('sample_date') ? strtotime($request->input('sample_date')) : time();
-            $code['sample_date'] = date('Y-m-d H:i:s', $sample_time);
-            $code['sample_code'] = SampleCode::generate_code();
-            $code['user_id']     = $request->user()->id;
-            $samplecode          = SampleCode::create($code);
+            try{
+                $code                = $request->only('hive_id', 'queen_id');
+                $sample_time         = $request->filled('sample_date') ? strtotime($request->input('sample_date')) : time();
+                $code['sample_date'] = date('Y-m-d H:i:s', $sample_time);
+                $code['sample_code'] = SampleCode::generate_code();
+                $code['user_id']     = $request->user()->id;
+                $samplecode          = SampleCode::create($code);
+            } catch (Exception $e) {
+                Log::error("Api/SampleCodeController.store error: ".$e->getMessage());
+            }
             return response()->json($samplecode, 201);
         }
         return response()->json(null, 400);

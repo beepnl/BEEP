f009e904f0a8ec45e3f4a862ea61bbd1ce5afbb0
ezja <change@this.com>
2022-12-21 10:23:57 +0100

    Revert "Add user specific item consent"
    This reverts commit ff023edb6edf1ab21f7c9ba7471c7321fddc8725.

--- app/Jobs/FlashLogJob.php
+++ app/Jobs/FlashLogJob.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Http\Request;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Throwable;
 
 class FlashLogJob implements ShouldQueue
 {
@@ -37,7 +38,12 @@ class FlashLogJob implements ShouldQueue
     public function handle(FlashLogController $flashLogController)
     {
 
-       FlashLogController::parse(FlashLogController::$flashlog);
+    // parse flashlog via Controller/Api/FlashlogController
+        FlashLogController::matchPercentage($array1, $array2, $match_props=9, $max_diff_percentage=0);
+        FlashLogController::parse(Request $request, $id, $persist=false, $delete=false);
+        FlashLogController::storeInfluxDataArrays($data_arrays, $device);
+        
+        return void;    
     }
 
     /**
@@ -50,6 +56,18 @@ class FlashLogJob implements ShouldQueue
         $queuedFlashLog = store(Request $request);
 
         FlashLogJob::dispatch($queuedFlashLog);    
+        $flash
     }
 
+    /*
+
+    queueFlashLog(function () use ($queuedFlashLog) {
+        $queuedFlashLog->handle();
+    })->catch(function (Throwable $e)
+    { 
+        // failed job
+    });
+
+    */
+    
 }

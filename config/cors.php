98c0bc38b86c419026d2422114f77254e0b25a4f
TheDiren <diren.senger@uni-bremen.de>
2023-03-17 11:24:44 +0100

    added login to cors paths

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['api/*', '*'],
+    'paths' => ['api/*', '*', 'login'],
 
     /*
     * Matches the request method. `['*']` allows all methods.

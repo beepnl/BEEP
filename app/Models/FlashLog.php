df8b2999537c471c5e72db7088fe2827783f6cc8
Pim van Gennip <pim@iconize.nl>
2024-10-01 10:20:27 +0200

    Fix undefined 'time_device'

--- app/Models/FlashLog.php
+++ app/Models/FlashLog.php
@@ -721,7 +721,7 @@ class FlashLog extends Model
         }
         
         // // If the device has an RTC, assume that all times match
-        if ($device->rtc == true)
+        if ($device->rtc == true && isset($flashlog[$start_index]['time_device']) && isset($flashlog[$end_index]['time_device']))
         {
             $end_moment  = new Moment($flashlog[$end_index]['time_device']);
             $time_end    = $end_moment->format($this->timeFormat);

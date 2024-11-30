0f2ff722da580ee7037db8ecc71730b00e939f05
Pim van Gennip <pim@iconize.nl>
2024-12-01 00:40:41 +0100

    Bugfix HiveLayer Log

--- app/HiveLayer.php
+++ app/HiveLayer.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Log;
 use Cache;
 
 class HiveLayer extends Model

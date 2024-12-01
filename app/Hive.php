601f42d0daf005225f97357568ceb34264557849
Pim van Gennip <pim@iconize.nl>
2024-12-01 02:09:22 +0100

    Update Hive.php

--- app/Hive.php
+++ app/Hive.php
@@ -17,7 +17,7 @@ class Hive extends Model
     protected $cascadeDeletes = ['queen','inspections','layers','frames'];
     protected $fillable = ['user_id', 'location_id', 'hive_type_id', 'color', 'name', 'bb_width_cm', 'bb_depth_cm', 'bb_height_cm', 'fr_width_cm', 'fr_height_cm', 'order'];
     protected $guarded  = ['id'];
-	protected $hidden 	= ['user_id','deleted_at'];
+	protected $hidden 	= ['user_id','deleted_at','pivot'];
     protected $appends  = ['type','location','attention','impression','notes','reminder','reminder_date','inspection_count','sensors','owner','editable','group_ids','last_inspection_date'];
 
     public $timestamps = false;

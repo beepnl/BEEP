83c1957e5d006d9c527e4a434ca09a52daea3ef5
Pim van Gennip <pim@iconize.nl>
2025-05-14 13:06:52 +0200

    Update ResearchController.php

--- app/Http/Controllers/ResearchController.php
+++ app/Http/Controllers/ResearchController.php
@@ -1241,7 +1241,7 @@ class ResearchController extends Controller
                     try{
                         $this->cacheRequestRate('influx-get');
                         $this->cacheRequestRate('influx-research');
-                        $query  = 'SELECT COUNT("bv") as "count" FROM "sensors" WHERE '.$user_device_keys.' AND time >= \''.$date_curr_consent.'\' AND time <= \''.$moment_end->format('Y-m-d H:i:s').'\' GROUP BY key,time(1d),from_flashlog';
+                        $query  = 'SELECT COUNT("bv") as "count" FROM "sensors" WHERE '.$user_device_keys.' AND time >= \''.$date_curr_consent.'\' AND time <= \''.$moment_end->format('Y-m-d H:i:s').'\' GROUP BY time(1d),key,from_flashlog';
                         //die($query); 
                         $points = $this->client::query($query)->getPoints();
 

65de796489bdc6b5af89a834958a3ce9cbfb6af9
Pim van Gennip <pim@iconize.nl>
2025-11-28 01:39:33 +0100

    ResearchData - Reparse Flashlogs that have no CSV

--- app/Http/Controllers/ResearchController.php
+++ app/Http/Controllers/ResearchController.php
@@ -1421,7 +1421,7 @@ class ResearchController extends Controller
                     foreach ($user_flashlogs as $fl)
                     {
                         // Add id's to reparse list
-                        if (!isset($fl->meta_data['rtc_bug']))
+                        if (!isset($fl->meta_data) || !isset($fl->csv_url))
                             $reparse_fl_ids[] = $fl->id;
 
                         $fl_dev_key = $fl->getDeviceKeyAttribute();

a339f9b924c3aa8ded84b6878becd9ea65324caa
Shift <shift@laravelshift.com>
2026-06-01 14:59:04 +0000

    Re-register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,14 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        \Laravel\Tinker\TinkerServiceProvider::class,
+        \EllipseSynergie\ApiResponse\Laravel\ResponseServiceProvider::class,
+        \Collective\Html\HtmlServiceProvider::class,
+        \Intervention\Image\ImageServiceProvider::class,
+        \Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
+        \TrayLabs\InfluxDB\Providers\ServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
--- config/app.php
+++ config/app.php
@@ -7,27 +7,6 @@ return [
 
     'webapp_url' => env('WEBAPP_URL', null),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-        Laravel\Tinker\TinkerServiceProvider::class,
-        EllipseSynergie\ApiResponse\Laravel\ResponseServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
-        Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
-        TrayLabs\InfluxDB\Providers\ServiceProvider::class,
-        //
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Form' => Collective\Html\FormFacade::class,

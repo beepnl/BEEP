6a67ffa5d8095da42a0c2856fc561a69e5c46aef
Shift <shift@laravelshift.com>
2026-06-02 10:06:52 +0000

    Dispatch jobs directly

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use DispatchesJobs;
 }
